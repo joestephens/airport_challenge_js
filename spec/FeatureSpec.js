4b75872e4da5a16299820f53b010db5b50c2dad4
Matt Butler <mattb-w@hotmail.co.uk>
2016-06-21 14:36:27 +0100

    added weather check for landing

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -29,7 +29,7 @@ describe('Feature Test:', function(){
 
   it('blocks landing when weather is stormy', function(){
     spyOn(airport,'isStormy').and.returnValue(true);
-    expect(function(){ plane.land(airport)}).toThrowError('cannot takeoff during storm');
+    expect(function(){ plane.land(airport)}).toThrowError('cannot land during storm');
     expect(airport.planes()).not.toContain(plane);
   });
 });
