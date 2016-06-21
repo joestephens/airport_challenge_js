a1756004488ad7e842b7135bd4ded50713f614e8
Matt Butler <mattb-w@hotmail.co.uk>
2016-06-21 14:31:57 +0100

    added weather check on takeoff

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -18,5 +18,18 @@ describe('Feature Test:', function(){
     plane.land(airport);
     plane.takeoff();
     expect(airport.planes()).not.toContain(plane);
-    });
+  });
+
+  it('blocks takeoff when weather is stormy', function(){
+    plane.land(airport)
+    spyOn(airport,'isStormy').and.returnValue(true);
+    expect(function(){ plane.takeoff();}).toThrowError('cannot takeoff during storm');
+    expect(airport.planes()).toContain(plane);
+  });
+
+  it('blocks landing when weather is stormy', function(){
+    spyOn(airport,'isStormy').and.returnValue(true);
+    expect(function(){ plane.land(airport)}).toThrowError('cannot takeoff during storm');
+    expect(airport.planes()).not.toContain(plane);
+  });
 });
