a59204d1b732b7cbe201e13b25f11949269aebc1
joestephens <joe.stephens@live.co.uk>
2016-06-22 11:50:58 +0100

    Plane

--- src/Plane.js
+++ src/Plane.js
@@ -4,6 +4,6 @@ Plane.prototype.land = function(airport){
   airport.clearForLanding(this);
   this._location = airport
 };
-Plane.prototype.takeoff = function(airport) {
+Plane.prototype.takeoff = function() {
   this._location.clearForTakeOff();
 };
